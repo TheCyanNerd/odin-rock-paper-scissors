ea02d4c86703326bf4e2be0389fa0a1d95287ca7
Salman Rashid <32942339+TheCyanNerd@users.noreply.github.com>
2023-05-21 16:19:32 -0400

    Added header and test for playRound()

--- script.js
+++ script.js
@@ -45,3 +45,10 @@ console.log(getComputerChoice());
 
 // Note: playerSelection should be handled case-insensitively
 
+function playRound(playerSelection, computerSelection) {
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
