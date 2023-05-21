821ab7ca416ec871092746a3f38ddc7e69085492
Salman Rashid <32942339+TheCyanNerd@users.noreply.github.com>
2023-05-21 15:39:26 -0400

    Wrote pseudocode for getComputerChoice()

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ console.log("Hello World!");
 //      1 = Paper
 //      2 = Scissors
 
+//  create a variable called randNum
+//  call Math.random() to generate a random number b/n 0 and 1
+//  multiply the result by 2 to get a   ""    ""    "" "" "" 2
+//  store that result in randNum
+//  create a variable called choiceStr
+//  use randNum's value to pick the correct choice to store in choiceStr
+//  return choiceStr
+
