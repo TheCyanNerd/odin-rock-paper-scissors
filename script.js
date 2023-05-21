15ac516a6669274ebd3f65f6a6f0efcede5f53a1
Salman Rashid <32942339+TheCyanNerd@users.noreply.github.com>
2023-05-21 15:33:18 -0400

    Wrote purpose statement for getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello World!");
+
+// getComputerChoice generates a random number between 0 and 2 (inclusive) and returns a string indicating a move
+
+//      0 = Rock
+//      1 = Paper
+//      2 = Scissors
+
